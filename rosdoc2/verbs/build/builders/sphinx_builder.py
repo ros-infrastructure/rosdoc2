1de6556cff639cf7b998804048a8a5afe5663854
R Kent James <kent@caspia.com>
2024-04-17 11:04:12 -0700

    Run sphinx-build as a module, ensures pipx compatibility (#99)
    * Run sphinx-build as a module, ensures pipx compatibility
    
    * Move pipx install to separate test

--- rosdoc2/verbs/build/builders/sphinx_builder.py
+++ rosdoc2/verbs/build/builders/sphinx_builder.py
@@ -19,6 +19,7 @@ import shutil
 import subprocess
 
 import setuptools
+from sphinx.cmd.build import main as sphinx_main
 
 from ..builder import Builder
 from ..collect_inventory_files import collect_inventory_files
@@ -566,17 +567,12 @@ class SphinxBuilder(Builder):
         # Invoke Sphinx-build.
         sphinx_output_dir = os.path.abspath(
             os.path.join(wrapped_sphinx_directory, 'sphinx_output'))
-        cmd = [
-            'sphinx-build',
-            wrapped_sphinx_directory,
-            sphinx_output_dir,
-        ]
         logger.info(
-            f"Running Sphinx-build: '{' '.join(cmd)}' in '{wrapped_sphinx_directory}'"
+            f"Running sphinx_build with: [{wrapped_sphinx_directory}, '{sphinx_output_dir}]'"
         )
-        completed_process = subprocess.run(cmd, cwd=wrapped_sphinx_directory)
-        msg = f"Sphinx-build exited with return code '{completed_process.returncode}'"
-        if completed_process.returncode == 0:
+        returncode = sphinx_main([wrapped_sphinx_directory, sphinx_output_dir])
+        msg = f"sphinx_build exited with return code '{returncode}'"
+        if returncode == 0:
             logger.info(msg)
         else:
             raise RuntimeError(msg)
